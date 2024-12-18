4d165e318f4862e37705ff087c6edce2cd9d98e6
Sebastian Hernandez <sebashr0920@gmail.com>
2024-12-18 13:47:52 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import {invoke} from "@tauri-apps/api/core";
 import "./App.css";
+import {useState} from "react";
 
 function App() {
+    const [open, setOpen] = useState(false);
+
+    async function addWebview() {
+        setOpen(true);
+        await invoke("plugin:dxwebview|create_webview", {payload: {value: "https://www.geogebra.org/classic"}});
+    }
+
+    async function removeWebview() {
+        setOpen(false);
+        // await invoke("plugin:dxwebview|remove_webview", {payload: {value: "https://www.geogebra.org/classic"}});
+    }
+
     return (
-        <button onClick={async () => {
-            await invoke("plugin:dxwebview|create_webview", {payload: {value: "https://www.geogebra.org/classic"}});
-        }}>
-            Greet
-        </button>
+        <main className={open ? "split" : "full"}>
+            <h1>POC split screen Android</h1>
+            <button onClick={() => addWebview()}>add</button>
+            <button onClick={() => removeWebview()}>remove</button>
+        </main>
     );
 }
 
